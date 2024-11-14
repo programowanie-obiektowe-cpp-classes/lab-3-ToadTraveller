725d44ed2e0b116a661be47dc08d42d29ad61dfb
ToadTraveller <toad.traveller@gmail.com>
2024-11-14 14:19:32 +0100

    Rozwiazane zadanie ze statkami

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,26 @@
 #pragma once
-
+using namespace std;
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+
+    if (towar == 0) return 0;
+
+    unsigned int sumTransport =0;
+    unsigned int liczbaZaglowcow = 0;
+    while (sumTransport < towar){
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();  
+        unsigned int przetransportowane = s1->transportuj();
+        sumTransport +=przetransportowane;
+
+        if (dynamic_cast<Zaglowiec*>(s1) != nullptr){
+            ++liczbaZaglowcow;
+        }
+        delete s1;
+    }
+    
+    
+    return liczbaZaglowcow;
 }
